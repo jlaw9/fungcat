c5ffbed414f7a4038ee458be6efb566642b848ea
jlaw9 <jeffreynlaw@gmail.com>
2017-11-15 17:03:23 -0500

    added more settings and functionality

--- src/fungcat_settings.py
+++ src/fungcat_settings.py
@@ -1,5 +1,8 @@
 # file to contain settings used by multiple scripts
 
+import utils.file_utils as utils
+import socket
+
 # Ouputs Structure:
 # ouputs - version1 - all - method 1 - results.txt
 #       |          |      - method 2 - results.txt
@@ -11,18 +14,41 @@ ALLOWEDVERSIONS = [
     "2017_10-seq-sim",
     "2017_10-string",
     "2017_10-seq-sim-string",
+    "2017_10-seq-sim-14",
+    "2017_10-seq-sim-x5-string-14",
+    "2017_10-seq-sim-x5-string",
+    "2017_10-string-14",
     ]
 
 SEQ_SIM_NETWORKS = {
     "2017_10-seq-sim": "inputs/protein-similarity/2017-10-08-shiv-similarity-network-uniprot.txt",
+    "2017_10-seq-sim-14": "inputs/protein-similarity/2017-10-08-shiv-similarity-network-uniprot.txt",
     "2017_10-seq-sim-string": "inputs/protein-similarity/2017-10-08-shiv-similarity-network-uniprot.txt",
+    "2017_10-seq-sim-x5-string-14": "inputs/protein-similarity/2017-10-08-shiv-similarity-network-uniprot.txt",
+    "2017_10-seq-sim-x5-string": "inputs/protein-similarity/2017-10-08-shiv-similarity-network-uniprot.txt",
     }
 
 NETWORK_VERSION_INPUTS = {
-        "2017_10-seq-sim": ['SEQ_SIM'],
-        "2017_10-seq-sim-string": ['SEQ_SIM', 'STRING'],
-        "2017_10-string": ['STRING'],
+    "2017_10-seq-sim": ['SEQ_SIM'],
+    "2017_10-seq-sim-14": ['SEQ_SIM'],
+    "2017_10-seq-sim-string": ['SEQ_SIM', 'STRING'],
+    "2017_10-seq-sim-x5-string-14": ['SEQ_SIM', 'STRING'],
+    "2017_10-seq-sim-x5-string": ['SEQ_SIM', 'STRING'],
+    "2017_10-string": ['STRING'],
+    "2017_10-string-14": ['STRING'],
     }
+
+
+VERSION_SELECTED_STRAINS = {
+    "2017_10-seq-sim": 'inputs/selected-strains.txt',
+    "2017_10-seq-sim-string": 'inputs/selected-strains.txt',
+    "2017_10-seq-sim-x5-string": 'inputs/selected-strains.txt',
+    "2017_10-string": 'inputs/selected-strains.txt',
+    "2017_10-seq-sim-14": 'inputs/selected-strains-14.txt',
+    "2017_10-seq-sim-x5-string-14": 'inputs/selected-strains-14.txt',
+    "2017_10-string-14": 'inputs/selected-strains-14.txt',
+}
+SELECTED_STRAINS = "inputs/selected-strains.txt"
 
 ALGORITHM_OPTIONS = {
     "local-ova": "--ova local",
@@ -41,29 +67,29 @@ ALGORITHM_OPTIONS = {
 # STRING networks are in the individual taxon dirs
 #STRING_TAXON_UNIPROT = "%s/%%s/%%s-uniprot-links-v10.5-%%d.txt" % (STRING_TAXON_DIR)
 
-TAX_TO_NAME = {
-    "Neisseria gonorrhoeae FA 1090"                             : "242231",
-    "Peptoclostridium difficile / Clostridioides difficile 630" : "272563",
-    "Helicobacter pylori 85962"                                 :  "85962",
-    "Klebsiella pneumoniae"                                     : "272620",
-    "Enterococcus faecium DO"                                   : "333849",
-    "Escherichia coli K-12"                                     :  "83333",
-    "Haemophilus influenzae RD KW20"                            :  "71421",
-    "Bordetella pertussis Tohama I"                             : "257313",
-    "Burkholderia cepacia"                                      : "269482",
-    "Clostridium botulinum A str. Hall"                         : "441771",
-    "Acinetobacter baumannii"                                   : "509170",
-    "Staphylococcus aureus"                                     :  "93061",
-    "Vibrio cholerae O1 biovar El Tor str. N16961"              : "243277",
-    "Yersinia pestis"                                           :    "632",
-    "Streptococcus pyogenes"                                    : "301447",
-    "Pseudomonas aeruginosa"                                    : "208964",
-    "Salmonella typhimurium / Salmonella enterica"              :  "99287",
-    "Shigella dysenteriae serotype 1 (strain Sd197)"            : "300267",
-    "Mycobacterium tuberculosis"                                :  "83332",
+NAME_TO_SHORTNAME = {
+    "Neisseria gonorrhoeae FA 1090"                             : "Ng",
+    "Peptoclostridium difficile / Clostridioides difficile 630" : "Cd",
+    "Helicobacter pylori 85962"                                 : "Hp",
+    "Klebsiella pneumoniae"                                     : "Kp",
+    "Enterococcus faecium DO"                                   : "Ef",
+    "Escherichia coli K-12"                                     : "Ec",
+    "Haemophilus influenzae RD KW20"                            : "Hi",
+    "Bordetella pertussis Tohama I"                             : "Bp",
+    "Burkholderia cepacia"                                      : "Bc",
+    "Clostridium botulinum A str. Hall"                         : "Cb",
+    "Acinetobacter baumannii"                                   : "Ab",
+    "Staphylococcus aureus"                                     : "Sa",
+    "Vibrio cholerae O1 biovar El Tor str. N16961"              : "Bc",
+    "Yersinia pestis"                                           : "Yp",
+    "Streptococcus pyogenes"                                    : "Sp",
+    "Pseudomonas aeruginosa"                                    : "Pa",
+    "Salmonella typhimurium / Salmonella enterica"              : "Se",
+    "Shigella dysenteriae serotype 1 (strain Sd197)"            : "Sd",
+    "Mycobacterium tuberculosis"                                : "Mt",
     }
-
-SELECTED_STRAINS = "inputs/selected-strains.txt"
+NAME_TO_TAX = {}
+TAX_TO_NAME = {}
 
 GOA_DIR = "/data/inputs/goa"
 GOA_TAXON_DIR = "%s/taxon" % (GOA_DIR)
@@ -98,25 +124,42 @@ STRING_TAXON_FILE = "%s/%%s/%%s.links.full.v10.5-%%d.txt" % (STRING_TAXON_DIR)
 # STRING FILE mapped to uniprot IDs
 STRING_TAXON_UNIPROT = "%s/%%s/%%s-uniprot-links-v10.5-%%d.txt" % (STRING_TAXON_DIR)
 
+# first column is uniprot ID, last is STRING ID
+UNIPROT_TO_STRING = "inputs/protein-similarity/uniprot-species/uniprot-proteins-19-strains-plus-string.tab"
+# first column is uniprot ID, second is organism ID
+UNIPROT_TO_SPECIES = "inputs/protein-similarity/uniprot-species/2017-10-17-uniprot-prots-19-species.tab"
+
 # code directory
-BIORITHM = "/home/jeffl/src/c++/biorithm/trunk"
+BIORITHM_ubuntu16 = "/home/jeffl/src/c++/biorithm/trunk"
+# use the ubuntu14 built version of GAIN on ubuntu 14 because the c++ packages are different
 BIORITHM_ubuntu14 = "/home/jeffl/src/c++/biorithm-ubuntu14/trunk"
+
+BIORITHM = BIORITHM_ubuntu16
+ubuntu14_hosts = ['spode', 'cuthbert']
+for host in ubuntu14_hosts:
+    if host in socket.gethostname():
+        BIORITHM = BIORITHM_ubuntu14
 
 VERSION = ''
 INPUTSPREFIX = ''
 RESULTSPREFIX = ''
 # processed network file for each version
-NETWORK = "inputs/%s/%s-net.txt"
+NETWORK_template = "inputs/%s/%s-net.txt"
 
 
 def set_version(version):
     global VERSION, INPUTSPREFIX, RESULTSPREFIX, NETWORK
+    global NAME_TO_TAX, TAX_TO_NAME
 
     VERSION = version
-    print "Using version %s" % (VERSION)
+    print "Using version '%s'" % (VERSION)
 
     INPUTSPREFIX = "inputs/%s" % VERSION
     RESULTSPREFIX = "outputs/%s" % VERSION
-    NETWORK = NETWORK % (VERSION, VERSION)
+    NETWORK = NETWORK_template % (VERSION, VERSION)
 
-    return INPUTSPREFIX, RESULTSPREFIX, NETWORK
+    selected_strains = utils.readItemSet(VERSION_SELECTED_STRAINS[VERSION], 1)
+    TAX_TO_NAME = utils.readDict(VERSION_SELECTED_STRAINS[VERSION], 1,2)
+    NAME_TO_TAX = utils.readDict(VERSION_SELECTED_STRAINS[VERSION], 2,1)
+
+    return INPUTSPREFIX, RESULTSPREFIX, NETWORK, selected_strains
